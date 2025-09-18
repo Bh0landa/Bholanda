bf14e7f9790e319ea4a963da56d04dd7eda48ea8
Holanda <bernardoholada@gmail.com>
2025-09-18 17:50:42 -0400

    19

--- pca/introdução/7.c
+++ pca/introdução/7.c
@@ -9,5 +9,10 @@ int main() {
         printf("%d\n", x[t]);
     }
 
+    for (t = 0; t < 10; t++) {
+        x[t] = (t * 2) - 1;
+        printf("%d\n", x[t]);
+    }
+
     return (0);
 }
--- pca/introdução/8.c
+++ pca/introdução/8.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#define TAMMAX 10
 int main() {
-    float vnota[10], media, soma = 0;
+    float v_nota[TAMMAX], media, soma = 0;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < TAMMAX; i++) {
         printf("informe a nota: ");
-        scanf("%f", &vnota[i]);
-        soma += vnota[i];
+        scanf("%f", &v_nota[i]);
+        soma += v_nota[i];
     }
-    media = soma / 10;
+    media = soma / TAMMAX;
+
+    for (int i = 0; i < TAMMAX; i++) {
+        if (v_nota[i] > media) {
+            printf("nota maior que a media: %.2f\n", v_nota[i]);
+        }
+    }
     printf("A media e: %.2f\n", media);
-    
+
     return (0);
 }
