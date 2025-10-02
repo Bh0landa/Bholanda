df358bbcafd15c3e334d355bb2e52c0852b4a1ae
Holanda <bernardoholada@gmail.com>
2025-10-02 17:13:10 -0400

    Create 6.c

---
+++ pca/lista/6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAX 100
+
+int main(){
+    int lista[MAX], qtd = 0, b, soma = 0;
+    float media;
+
+    while(qtd < MAX) {
+        printf("Digite um valor positivo e inteiro (-1 para parar): ");
+        scanf("%d", &b);
+        if (b == -1) break;
+        if (b > 0 && (int)b == b) {
+            lista[qtd] = b;
+            qtd++;
+        }
+        else{
+            printf("Valor invalido!\n");
+            qtd--;
+            continue;
+        }
+    }
+
+    printf("Maior valor %d\n", lista[0]);
+    for (int i = 1; i < qtd; i++) {
+        if (lista[i] > lista[0]) {
+            lista[0] = lista[i];
+        }
+    for(int i = 0; i < qtd; i++) {
+        soma += lista[i];
+    }
+    media = soma / qtd;
+    printf("Media dos valores inseridos: %.2f\n", media);
+    }
+    return 0;
+}
